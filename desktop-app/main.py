51c6847e10dd59deb5eb18156e5248860e47c95b
xenx <atvorozhkov@yandex.ru>
2016-06-16 23:14:14 -0300

    Made confuse work, made --logout work.

--- desktop-app/main.py
+++ desktop-app/main.py
@@ -3,6 +3,10 @@ from matrix_client.api import MatrixRequestError
 import confuse
 import argparse
 
+config = confuse.Configuration('graddfil')
+parser = argparse.ArgumentParser(description='Graddfil desktop app')
+parser.add_argument('--logout', dest='logout', action='store_true', default=False,
+                    help='Logout from current user')
 server_url = "https://matrix.org"  # without '/'
 
 def get_token(username, password):
@@ -15,30 +19,29 @@ def get_token(username, password):
         raise NameError('Your password or login did\'t mutch')
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Graddfil desktop app')
-
-    # We need to logout user
-    parser.add_argument('--logout', dest='logout', action='store_true', default=False,
-                                            help='Logout from current user')
-
-    config = confuse.Configuration('graddfil-desktop')
-
-    if 'token' not in config or parser.parse_args()['logout']:
+def login():
+    '''
+    Login user with token if there\s no token get username and password.
+    Will return MatrixHttpApi
+    '''
+    # Check for token
+    if 'token' not in config or parser.parse_args().logout:
         import getpass
         username = input('Please specify your username:')
         password = getpass.getpass()
 
         try:
+            # Set token and save config
             config['token'].set(get_token(username, password))
+            open(config.user_config_path(), 'w').write(config.dump())
         except MatrixRequestError:
-            print('Your password or username didn\'t match')
-            main()
-            return
+            print('Your password or username didn\'t match, please try again.')
+            login()
+    return MatrixHttpApi(server_url, token=config['token'].get())
 
-    matrix = MatrixHttpApi(server_url, token=config['token'].get())
+def main():
+    matrix = login()
     response = matrix.send_message("!dppXJSqykoVCKXEPcO:matrix.org", "Hello from python SDK!")
-    config.dump(redact=True)
 
 if __name__ == '__main__':
         main()
