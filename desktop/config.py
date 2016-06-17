3857c59ed8f6d9ba5d6090dd3b74d1c7dca5cf3c
xenx <atvorozhkov@yandex.ru>
2016-06-17 15:43:21 -0300

    Add config.py with Graddfril class. Now you can send massage with Graddfril.send_massage(*your massage*)

---
+++ desktop/config.py
@@ -0,0 +1,52 @@
+from matrix_client.api import MatrixHttpApi
+from matrix_client.api import MatrixRequestError
+import confuse
+import argparse
+
+config = confuse.Configuration('graddfil')
+parser = argparse.ArgumentParser(description='Graddfil desktop app')
+parser.add_argument('--clear-data', dest='wipe', action='store_true', default=False,
+                    help='Reset data. (server, token, room)')
+
+
+class Graddfril:
+    '''
+    :param token: Matrix token.
+    :param server: Matrix server to use.
+    :param room: Room id for sending data.
+    '''
+
+    def __init__(self):
+        '''
+        We need to check configuration file.
+        If room, host, token not in configuration file we ask to input them.
+        '''
+
+        for name in ['server', 'room']:
+            if name not in config or parser.parse_args().wipe:
+                config[name].set(input('Please specify %s:' % name))
+
+        self.server = config['server'].get()
+        self.room = config['room'].get()
+
+        if 'token' not in config or parser.parse_args().wipe:
+            import getpass
+            try:
+
+                login_information = MatrixHttpApi(self.server).login("m.login.password",
+                                                                     user=input("Please specify your username:"),
+                                                                     password=getpass.getpass())
+                if 'access_token' in login_information:
+                    config['token'].set(login_information['access_token'])
+                else:
+                    raise NameError("Your password or login did't much.")
+
+                # Save config.
+                open(config.user_config_path(), 'w').write(config.dump())
+            except MatrixRequestError:
+                print("Your password or username didn't match, please try again.")
+
+        self.token = config['token'].get()
+
+    def send_message(self, data):
+        MatrixHttpApi(self.server, token=self.token).send_message(self.room, data)
--- desktop/main.py
+++ desktop/main.py
@@ -1,48 +1,10 @@
-from matrix_client.api import MatrixHttpApi
-from matrix_client.api import MatrixRequestError
-import confuse
-import argparse
+from desktop.config import Graddfril
 
-config = confuse.Configuration('graddfil')
-parser = argparse.ArgumentParser(description='Graddfil desktop app')
-parser.add_argument('--logout', dest='logout', action='store_true', default=False,
-                    help='Logout from current user')
-
-server_url = "https://matrix.org"  # without '/'
-
-def get_token(username, password):
-    'Get Matrix token by user and password'
-    login_information = MatrixHttpApi(server_url).login("m.login.password",
-                    user=username, password=password)
-    if 'access_token' in login_information:
-        return login_information['access_token']
-    else:
-        raise NameError('Your password or login did\'t mutch')
-
-def login():
-    '''
-    Login user with token if there\s no token get username and password.
-    Will return MatrixHttpApi
-    '''
-
-    # Check for token
-    if 'token' not in config or parser.parse_args().logout:
-        import getpass
-        username = input('Please specify your username:')
-        password = getpass.getpass()
-
-        try:
-            # Set token and save config
-            config['token'].set(get_token(username, password))
-            open(config.user_config_path(), 'w').write(config.dump())
-        except MatrixRequestError:
-            print('Your password or username didn\'t match, please try again.')
-            login()
-    return MatrixHttpApi(server_url, token=config['token'].get())
 
 def main():
-    matrix = login()
-    matrix.send_message("!dppXJSqykoVCKXEPcO:matrix.org", "Hello from python SDK!")
+    matrix = Graddfril()
+    matrix.send_message("Hello from python SDK!")
+
 
 if __name__ == '__main__':
     main()
