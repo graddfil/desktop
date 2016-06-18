c56c43928953616ff310946b31c26ce3f6ff9218
Dmitriy Volkov <wldhx+vcs+github_com@wldhx.me>
2016-06-18 12:18:56 +0300

    Refactor
    Rename configure/{Graddfril -> MatrixClient}
    Start separating concerns:
     - provide a "normal" constructor for MatrixClient
     - move init from config into `from_config(config)`
    Dependency injection for `from_config()`

--- desktop/configure.py
+++ desktop/configure.py
@@ -9,14 +9,20 @@ parser.add_argument('--clear-data', dest='wipe', action='store_true', default=Fa
                     help='Reset data. (server, token, room)')
 
 
-class Graddfril:
+class MatrixClient:
     '''
-    :param token: Matrix token.
     :param server: Matrix server to use.
+    :param token: Matrix token.
     :param room: Room id for sending data.
     '''
 
-    def __init__(self):
+    def __init__(self, server: str, token: str, room: str):
+        self.server = server
+        self.token = token
+        self.room = room
+
+
+    def from_config(config: confuse.Configuration):
         '''
         We need to check configuration file.
         If room, host, token not in configuration file we ask to input them.
@@ -26,14 +32,14 @@ class Graddfril:
             if name not in config or parser.parse_args().wipe:
                 config[name].set(input('Please specify %s:' % name))
                 open(config.user_config_path(), 'w').write(config.dump())
-        self.server = config['server'].get()
-        self.room = config['room'].get()
+        server = config['server'].get()
+        room = config['room'].get()
 
         if 'token' not in config or parser.parse_args().wipe:
             import getpass
             try:
 
-                login_information = MatrixHttpApi(self.server).login("m.login.password",
+                login_information = MatrixHttpApi(server).login("m.login.password",
                                                                      user=input("Please specify your username:"),
                                                                      password=getpass.getpass())
                 if 'access_token' in login_information:
@@ -46,7 +52,9 @@ class Graddfril:
             except MatrixRequestError:
                 print("Your password or username didn't match, please try again.")
 
-        self.token = config['token'].get()
+        token = config['token'].get()
+
+        return MatrixClient(server, token, room)
 
     def send_message(self, data):
         print(MatrixHttpApi(self.server, token=self.token).send_message(self.room, data))
--- desktop/main.py
+++ desktop/main.py
@@ -1,8 +1,8 @@
-from configure import Graddfril
+from configure import config, MatrixClient
 
 
 def main():
-    matrix = Graddfril()
+    matrix = MatrixClient.from_config(config)
     matrix.send_message("Hello world!")
 
 if __name__ == '__main__':
