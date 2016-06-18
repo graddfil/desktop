36daef1ee777c9edcb293a4a81999cac046cfa17
Dmitriy Volkov <wldhx+vcs+github_com@wldhx.me>
2016-06-18 18:18:14 +0300

    Refactor: style

--- desktop/matrix.py
+++ desktop/matrix.py
@@ -16,7 +16,6 @@ class MatrixClient:
         self.api = MatrixHttpApi(self.server, token=self.token)
         self.api.initial_sync()
 
-
     @classmethod
     def from_config(cls, config):
         server = config['server'].get()
@@ -25,13 +24,11 @@ class MatrixClient:
 
         return cls(server, token, room)
 
-
     def send_event(self, event_type, content):
         return self.api.send_message_event(self.room, event_type, content)
-
 
     @staticmethod
     def get_token(server: str, user: str, password: str) -> str:
         return MatrixHttpApi(server).login("m.login.password",
-                user=user,
-                password=password)
+                                           user=user,
+                                           password=password)
